b31a97bd5333908b77cab7e63d98f7fdfec947ca
Yash <thisisyashs@gmail.com>
2023-08-20 22:29:39 +0530

    Exchanges button remove

--- src/App.jsx
+++ src/App.jsx
@@ -41,7 +41,7 @@ const App = () => (
         </Typography.Title>
         <Space>
           <Link to="/">Home</Link>
-          <Link to="/exchanges">Exchanges</Link>
+          <Link to="/Cryptocurrencies">Crypto-Currencies</Link>
           <Link to="/news">News</Link>
         </Space>
       </div>
